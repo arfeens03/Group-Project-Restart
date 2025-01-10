049584ddb9ac0f73c24556daa9515d2efcb49e6c
arfeens03 <bigheddoy@gmail.com>
2025-01-10 16:46:37 +0000

    Написал remote

---
+++ remote/server.go
@@ -0,0 +1,43 @@
+package remote
+
+func SendMain(request string) string {
+	// открываем соединение
+	/*
+		conn, err := net.Dial("tcp", ":8080")
+		if err != nil {
+			log.Panic("Error in remote: ", err)
+
+		}
+		defer conn.Close()
+		// отправляем данные
+		if _, err = conn.Write([]byte(request)); err != nil {
+			log.Panic(err)
+			return "Ошибка подключения"
+		}
+		connReader := bufio.NewReader(conn)
+		message, _ := connReader.ReadString('\n')
+		return message
+	*/
+	return "Ошибка подключения"
+}
+
+func SendAu(request string) string {
+	// открываем соединение
+	/*
+		conn, err := net.Dial("tcp", ":8080")
+		if err != nil {
+			log.Panic("Error in remote: ", err)
+			return "1"
+		}
+		defer conn.Close()
+		// отправляем данные
+		if _, err = conn.Write([]byte(request)); err != nil {
+			log.Panic(err)
+
+		}
+		connReader := bufio.NewReader(conn)
+		message, _ := connReader.ReadString('\n')
+		return message
+	*/
+	return "1"
+}
